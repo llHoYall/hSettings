6d35b07cb33188b928fb17f68b1cb53524702661
llChameleoNll <hoya128@gmail.com>
2017-03-06 11:25:20 +0900

    Add zsh setting

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Install RVM (Ruby Version Manager)
+curl -sSL https://get.rvm.io | sudo bash -s stable
+
 # Install zsh
 sudo apt-get install zsh
 chsh -s `which zsh`
@@ -7,3 +10,5 @@ chsh -s `which zsh`
 # Install oh-my-zsh
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 
+# Setting file
+cp .zshrc ~/

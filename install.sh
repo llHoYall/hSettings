383fd839ae99edcc79c2214d0bea2ca75e55615b
llChameleoNll <hoya128@gmail.com>
2017-04-18 10:22:45 +0900

    Update install script

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# Color Value ##################################################################
 yellow='\033[0;33m'
 nc='\033[0m'
 
+# usage function ###############################################################
 function usage() {
 	echo "for MAC or Linux"
 	echo "  usage: ./install.sh [all | zsh | git | vim | vscode]"
@@ -10,6 +12,7 @@ function usage() {
 	echo "  usage: sh ./install.sh [all | zsh | git | vim | vscode]"
 }
 
+# check OS #####################################################################
 os=None
 if [ "$(uname -s)" == "Darwin" ]; then
 	os=MAC
@@ -22,6 +25,7 @@ else
 	exit 1
 fi
 
+# check Argument ###############################################################
 args=None
 if [[ $# -ne 1 ]]; then
 	usage
@@ -29,5 +33,14 @@ if [[ $# -ne 1 ]]; then
 fi
 args=$1
 
-echo -e "=== Install ${yellow}${args}${nc} on ${yellow}${os}${nc} ==="
+# print Message ################################################################
+if [ $os = MAC -o $os = LINUX ]; then
+	echo -e "=== Install ${yellow}${args}${nc} on ${yellow}${os}${nc} ==="
+elif [ $os = WINDOWS ]; then
+	write-host -NoNewline "=== Install "
+	write-host -NoNewline -ForegroundColor "Yellow" $args
+	write-host -NoNewline " on "
+	write-host -NoNewline -ForegroundColor "Yellow" $os
+	write-host " ==="
+fi
 

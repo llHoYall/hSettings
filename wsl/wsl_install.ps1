7155f7c5f0717f1ac6857c373372e2e3e8fb0179
llChameleoNll <hoya128@gmail.com>
2017-10-15 20:33:56 +0900

    [WSL] Add settings

--- wsl/wsl_install.ps1
+++ wsl/wsl_install.ps1
@@ -10,6 +10,14 @@ else {
 <# Download Wsl-Terminal -----------------------------------------------------#>
 $url = "https://github.com/goreliu/wsl-terminal/releases/download/v0.8.3/wsl-terminal-0.8.3.zip"
 $output = "$home\wsl-terminal-0.8.3.zip"
+if (Get-Command open-wsl -errorAction SilentlyContinue) {
+	(New-Object System.Net.WebClient).DownloadFile($url, $output)
+	Expand-Archive $output -Destination "$home"
+}
 
-(New-Object System.Net.WebClient).DownloadFile($url, $output)
-Expand-Archive $output -Destination "$home"
+<# Setting Wsl-Terminal ------------------------------------------------------#>
+Write-Host $path
+Write-Host $home
+
+Copy-Item $path\wsl\wsl-terminal.conf $home\wsl-terminal\etc
+Copy-Item $path\wsl\minttyrc $home\wsl-terminal\etc

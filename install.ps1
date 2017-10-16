897955edda3337d23391e14d67cfbbe69cde14a8
llChameleoNll <hoya128@gmail.com>
2017-10-16 13:11:20 +0900

    [Common] Refactoring

--- install.ps1
+++ install.ps1
@@ -1,18 +1,15 @@
 <# Usage Function ------------------------------------------------------------#>
 function usage {
-	Write-Host ""
+	Write-Host
 	Write-Host "[Usage]"
 	Write-Host "for Windows"
 	Write-Host "  usage: .\install.ps1 [list of tools]"
-	Write-Host "  list of supported tools"
+	Write-Host "    list of supported tools"
 	Write-Host "    - all"
 	Write-Host "    - wsl" 
-	Write-Host "    - git" 
-	Write-Host "    - wsl" 
-	Write-Host "    - vscode" 
-	Write-Host "  ex) $ .\install.ps1 all"
-	Write-Host "  ex) $ .\install.ps1 wsl git"
-	Write-Host ""
+	Write-Host "  ex: .\install.ps1 all"
+	Write-Host "  ex: .\install.ps1 wsl"
+	Write-Host
 }
 
 <# Check PowerShell Version --------------------------------------------------#>
@@ -22,39 +19,38 @@ if ($PSVersionTable.PSVersion -ge "5.0") {
 else {
 	Write-Host -ForegroundColor "red" "Error: Not supported PowerShell version"
 	Write-Host
-	Exit(2)
+	Exit(1)
 }
 
 <# Check Argument ------------------------------------------------------------#>
-if (($Args.Count -ne 1) `
-	-Or (($Args -ne "all") -And ($Args -ne "wsl") -And ($Args -ne "git") -And ($Args -ne "vim"))) {
-	Write-Host -NoNewline -ForegroundColor "red" "Error: Not supported tool"
+if (($Args.Count -lt 1) -Or ($Args.Count -gt 1)) {
+	Write-Host
+	Write-Host -NoNewline -ForegroundColor "red" "Error: Invalid arguments"
 	usage
-	Exit(1)
-}
+	Exit(2)
+} 
 Write-Host
 
 <# Install Tools -------------------------------------------------------------#>
 $path=$Pwd.path
 cd $PSScriptRoot
 
-# wsl
-if (($Args[0] -eq "all") -or ($Args[0] -eq "wsl")) {
-	Write-Host -NoNewline "==> Install "
-	Write-Host -ForegroundColor "Yellow" "wsl"
-	wsl/wsl_install.ps1
+$tools = @($Args)
+If (($Args.Count -eq 1) -And ($Args -eq "all")) {
+	$tools = @("wsl")
 }
 
-switch ($Args[0]) {
-	"git" {
-		git/git_setting.ps1 $path
-	}
-	"vim" {
-		vim/vim_install.ps1
-	}
-	default {
-		Write-Host -ForegroundColor "red" "Error: Not supported tool"
-		Write-Host
-		Exit(2)
+For ($i = 0; $i -lt $tools.Length; $i++) {`
+	Write-Host -NoNewline "==> Install "
+	Write-Host -ForegroundColor YELLOW $tools[$i]
+	switch ($tools[$i]) {
+		"wsl" {
+			wsl/wsl_install.ps1
+		 }
+		default {
+			Write-Host -ForegroundColor RED "Error: Not supported tool"
+			Write-Host
+			Exit(3)
+		}
 	}
 }

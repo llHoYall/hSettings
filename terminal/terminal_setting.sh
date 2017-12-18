0e346b99b85c05820e2aa92c0cf1acb1dea476cd
llChameleoNll <hoya128@gmail.com>
2017-12-18 16:30:12 +0900

    [terminal] Update tmux

--- terminal/terminal_setting.sh
+++ terminal/terminal_setting.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
 
 # tmux Configuration ----------------------------------------------------------#
-if [ -z "$(command -v git)" ]; then
-	sudo apt-get install git -y
-fi
-if ! test -d ~/.tmux; then
-	cd ~
-	git clone https://github.com/gpakosz/.tmux
-else
-	cd ~/.tmux
-	git pull
-	cd -
-fi
-ln -s -f .tmux/.tmux.conf
-cp .tmux.conf.local ~/
+cp .tmux.conf ~/

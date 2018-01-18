2d744ed5fd6cb805e36643beadc95939746e3066
llHoYall <hoya128@gmail.com>
2018-01-18 16:59:21 +0900

    [hConfig] Updating

--- shell/shell.py
+++ shell/shell.py
@@ -12,140 +12,34 @@
 # Built-In
 import os
 import sys
-import shutil
-import pathlib
+import platform
 import subprocess
 # User
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from misc import color  # noqa
+import zsh              # noqa
 
 
 # Install --------------------------------------------------------------------#
-def install_mac():
+def install(hos):
+    print("==> Install " + color.ORANGE + "shell" + color.END)
+
     # zsh
-    print("  => Install " + color.ORANGE + "zsh" + color.END)
-    if os.system("which zsh 1> /dev/null"):
-        os.system("brew install zsh zsh-completions")
-        os.system("chsh -s $(which zsh)")
-    else:
-        print("       Already installed.")
-
-    # oh-my-zsh
-    print("  => Install " + color.ORANGE + "oh-my-zsh" + color.END)
-    dir = str(pathlib.Path.home()) + "/.oh-my-zsh"
-    if os.path.isdir(dir):
-        os.system("curl -fsSL https\://raw.github.com/robbyrussell/"
-                  "oh-my-zsh/master/tools/install.sh | sh")
-
-        # zsh-syntax-highlighting plugin
-        dir = str(pathlib.Path.home())
-        dir += "/.oh-my-zsh/plugins/zsh-syntax-highlighting"
-        if not os.path.isdir(dir):
-            os.system("git clone https\://github.com/zsh-users/"
-                      "zsh-syntax-highlighting ~/.oh-my-zsh/plugins/"
-                      "zsh-syntax-highlighting")
-
-        # zsh-autosuggestions plugin
-        dir = str(pathlib.Path.home())
-        dir += "/.oh-my-zsh/plugins/zsh-autosuggestions"
-        if not os.path.isdir(dir):
-            os.system("git clone https\://github.com/zsh-users/"
-                      "zsh-autosuggestions ~/.oh-my-zsh/plugins/"
-                      "zsh-autosuggestions")
-    else:
-        print("       Already installed.")
-
-
-def install_linux():
-    # zsh
-    print("  => Install " + color.ORANGE + "zsh" + color.END)
-    if os.system("which zsh 1> /dev/null"):
-        os.system("sudo apt install zsh")
-        os.system("chsh -s $(which zsh)")
-    else:
-        print("       Already installed.")
-
-    # oh-my-zsh
-    print("  => Install " + color.ORANGE + "oh-my-zsh" + color.END)
-    dir = str(pathlib.Path.home()) + "/.oh-my-zsh"
-    if not os.path.isdir(dir):
-        os.system("curl -fsSL https\://raw.github.com/robbyrussell/"
-                  "oh-my-zsh/master/tools/insstall.sh | sh")
-
-        # zsh-completions plugin
-        dir = str(pathlib.Path.home()) + "/.oh-my-zsh/plugins/zsh-completions"
-        if not os.path.isdir(dir):
-            os.system("git clone https\://github.com/zsh-users/"
-                      "zsh-completions ~/.oh-my-zsh/plugins/"
-                      "zsh-completions")
-
-        # zsh-syntax-highlighting plugin
-        dir = str(pathlib.Path.home())
-        dir += "/.oh-my-zsh/plugins/zsh-syntax-highlighting"
-        if not os.path.isdir(dir):
-            os.system("git clone https\://github.com/zsh-users/"
-                      "zsh-syntax-highlighting ~/.oh-my-zsh/plugins/"
-                      "zsh-syntax-highlighting")
-
-        # zsh-autosuggestions plugin
-        dir = str(pathlib.Path.home())
-        dir += "/.oh-my-zsh/plugins/zsh-autosuggestions"
-        if not os.path.isdir(dir):
-            os.system("git clone https\://github.com/zsh-users/"
-                      "zsh-autosuggestions ~/.oh-my-zsh/plugins/"
-                      "zsh-autosuggestions")
-    else:
-        print("       Already installed.")
+    if hos == 'Darwin' or hos == 'Linux':
+        zsh.install(hos)
 
 
 # Config ---------------------------------------------------------------------#
-def config_mac():
+def config(hos):
+    print("==> Config " + color.ORANGE + "shell" + color.END)
+
     # zsh
-    print("  => Configure " + color.ORANGE + "zsh" + color.END)
-    if os.system("which zsh 1> /dev/null"):
-        print("       Not installed.")
-    else:
-        src = os.path.dirname(os.path.realpath(__file__)) + "/.zshrc"
-        dst = str(pathlib.Path.home())
-        if os.path.isfile(src):
-            shutil.copy2(src, dst)
-
-    # oh-my-zsh
-    print("  => Configure " + color.ORANGE + "oh-my-zsh" + color.END)
-    dir = str(pathlib.Path.home()) + "/.oh-my-zsh"
-    if os.path.isdir(dir):
-        src = os.path.dirname(os.path.realpath(__file__)) + "/hoya.zsh-theme"
-        dst = str(pathlib.Path.home()) + "/.oh-my-zsh/themes"
-        if os.path.isfile(src):
-            shutil.copy2(src, dst)
-    else:
-        print("       Not installed.")
-
-    print("You must type " + color.GREEN + "'source ~/.zshrc'" + color.END +
-          " to apply configuration.")
+    if hos == 'Darwin' or hos == 'Linux':
+        zsh.config(hos)
 
 
-def config_linux():
-    # zsh
-    print("  => Configure " + color.ORANGE + "zsh" + color.END)
-    if os.system("which zsh 1> /dev/null"):
-        print("       Not installed.")
-    else:
-        src = os.path.dirname(os.path.realpath(__file__)) + "/.zshrc"
-        dst = str(pathlib.Path.home())
-        if os.path.isfile(src):
-            shutil.copy2(src, dst)
-
-    # oh-my-zsh
-    print("  => Configure " + color.ORANGE + "oh-my-zsh" + color.END)
-    dir = str(pathlib.Path.home()) + "/.oh-my-zsh"
-    if os.path.isdir(dir):
-        src = os.path.dirname(os.path.realpath(__file__)) + "/hoya.zsh-theme"
-        dst = str(pathlib.Path.home()) + "/.oh-my-zsh/themes"
-        if os.path.isfile(src):
-            shutil.copy2(src, dst)
-    else:
-        print("       Not installed.")
-
-    print("You must type " + color.GREEN + "'source ~/.zshrc'" + color.END +
-          " to apply configuration.")
+# Main Routine ---------------------------------------------------------------#
+if __name__ == '__main__':
+    hos = platform.system()
+    install(hos)
+    config(hos)
--- test.py
+++
@@ -1,8 +0,0 @@
-import os
-import pathlib
-path = str(pathlib.Path.home()) + "/.oh-my-zsh"
-print(path)
-if os.path.isdir(path):
-    print("OK")
-else:
-    print("NO")

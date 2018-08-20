ced1e7c99ca88077fdab29c3c8b6aad5878a7bf3
llHoYall <hoya128@gmail.com>
2018-08-20 12:25:33 +0000

    Update for Linux (Ubuntu)

--- git/git_install.sh
+++ git/git_install.sh
@@ -30,7 +30,6 @@ if [ -z "$(command -v git-lfs)" ]; then
 	if [ "$(uname -s)" == "Darwin" ]; then
 		brew install git-lfs
 	elif [ "$(uname -s)" == "Linux" ]; then
-		curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
 		sudo apt install -y git-lfs
 	fi
 else

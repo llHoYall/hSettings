f3b5835adc665d23dbdd1849c5921f7eac3b1d3f
llHoYall <hoya128@gmail.com>
2018-09-21 12:08:47 +0900

    Update for Linux

--- devtool/web/web_install.sh
+++ devtool/web/web_install.sh
@@ -17,7 +17,10 @@ if [ -z "$(command -v nvm)" ]; then
 	if [ "$(uname -s)" == "Darwin" ]; then
 		brew install nvm
 	elif [ "$(uname -s)" == "Linux" ]; then
-		sudo apt install -y nvm
+		curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+		export NVM_DIR="$HOME/.nvm"
+		[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+		[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 	fi
 else
 	echo "    Already installed"

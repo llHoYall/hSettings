a638647d5ec2e3ac8270288e84689f6ab0292900
llHoYall <hoya128@gmail.com>
2019-03-16 21:36:38 +0900

    Update for Windows

--- shell/powershell/Microsoft.PowerShell_profile.ps1
+++ shell/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,6 @@
+<# Default Starting Location -------------------------------------------------#>
+# Set-Location $HOME
+
 <# Chocolatey Profile --------------------------------------------------------#>
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
@@ -31,10 +34,21 @@ $global:GitPromptSettings.AfterText  = '] '
 Import-Module -Name PSReadLine
 
 <# Aliasing ------------------------------------------------------------------#>
+# Set-Alias vim 'C:\Program Files (x86)\vim\vim80\vim.exe'
+Set-Alias vim 'C:\tools\neovim\Neovim\bin\nvim.exe'
 Set-Alias meld 'C:\Program Files (x86)\Meld\Meld.exe'
 Set-Alias python2 'C:\Python27\python.exe'
 Set-Alias python3 'C:\Python37\python.exe'
 Set-Alias python python3
+
+function Git-Add { git add $args }
+Set-Alias -Name gad -Value Git-Add
+
+function Git-Commit { git commit $args }
+Set-Alias -Name gcmm -Value Git-Commit
+
+function Git-Checkout { git checkout $args }
+Set-Alias -Name gco -Value Git-Checkout
 
 function Git-Status { git status $args }
 Set-Alias -Name gst -Value Git-Status

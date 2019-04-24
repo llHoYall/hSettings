cfb010dc6b12279fbbd50b4a83210f9f7d213ba9
llHoYall <hoya128@gmail.com>
2019-04-24 16:50:16 +0900

    Update Windows - ConEmu

--- terminal/conemu/conemu_config.ps1
+++ terminal/conemu/conemu_config.ps1
@@ -11,10 +11,14 @@
 Write-Host -NoNewline " => Config "
 Write-Host -ForegroundColor YELLOW "ConEmu"
 If (Test-Path -Path "C:\Program Files\ConEmu") {
-	If (Test-Path "C:\Users\$($env:UserName)\AppData\Roaming\conemu_sexy.xml") {
-		Remove-Item "C:\Users\$($env:UserName)\AppData\Roaming\conemu_sexy.xml"
+	# If (Test-Path "C:\Users\$($env:UserName)\AppData\Roaming\conemu_sexy.xml") {
+	# 	Remove-Item "C:\Users\$($env:UserName)\AppData\Roaming\conemu_sexy.xml"
+	# }
+	# cmd /c mklink /H "C:\Users\$($env:UserName)\AppData\Roaming\conemu_sexy.xml" "$($PSScriptRoot)\conemu_sexy.xml"
+	If (Test-Path "C:\Program Files\ConEmu\conemu_sexy.xml") {
+		Remove-Item "C:\Program Files\ConEmu\conemu_sexy.xml"
 	}
-	cmd /c mklink /H "C:\Users\$($env:UserName)\AppData\Roaming\conemu_sexy.xml" "$($PSScriptRoot)\conemu_sexy.xml"
+	cmd /c mklink /H "C:\Program Files\ConEmu\conemu_sexy.xml" "$($PSScriptRoot)\conemu_sexy.xml"
 }
 Else {
 	Write-Host "    Not installed."
